34f00fe32a015010c131edf588378e478ce13520
Julien Levesy <jlevesy@gmail.com>
2015-04-02 19:34:10 +0200

    run rabbitmq-plugins enable on Docker build

--- rabbitmq/run.sh
+++ rabbitmq/run.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-confd -verbose -interval=30 -backend etcd -node 172.17.42.1:4001 -config-file /etc/confd/conf.d/hosts.toml &
 confd -verbose -onetime -backend etcd -node 172.17.42.1:4001 -config-file /etc/confd/conf.d/rabbitmq.toml
-rabbitmq-plugins enable rabbitmq_management
+confd -verbose -interval=30 -backend etcd -node 172.17.42.1:4001 -config-file /etc/confd/conf.d/hosts.toml &
 rabbitmq-server
