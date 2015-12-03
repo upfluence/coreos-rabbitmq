e93c51caf5e3aad99ac3feac9699820f5fdc6a15
Julien Levesy <jlevesy@gmail.com>
2015-12-03 11:25:55 +0100

    use exec in run.sh

--- rabbitmq/run.sh
+++ rabbitmq/run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 confd -verbose -onetime -backend etcd -node 172.17.42.1:4001 -config-file /etc/confd/conf.d/rabbitmq.toml
 confd -verbose -interval=30 -backend etcd -node 172.17.42.1:4001 -config-file /etc/confd/conf.d/hosts.toml &
-rabbitmq-server
+exec rabbitmq-server
